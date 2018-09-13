d745ba601c622c94bbdda769b41919237a05ad7f
fran <zhanfan930818@hotmail.com>
2018-09-13 22:47:31 +0800

    Add Configuration Sample

---
+++ Asp.net core sample/Configuration/ConfigurationBuilder/HeaderTitle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationBuilder_Sample
+{
+    class HeaderTitle
+    {
+        public string Title { get; set; }
+        public int Width { get; set; }
+    }
+}
---
+++ Asp.net core sample/Configuration/ConfigurationBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConfigurationBuilder_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("json1.json");
+
+            var config = builder.Build();
+            var list= config.GetSection("report") .GetSection("user").GetChildren();
+
+            List<HeaderTitle> titles = new List<HeaderTitle>();
+            foreach (var item in list)
+            {
+                titles.Add(new HeaderTitle
+                {
+                    Title = item["title"],
+                    Width = Convert.ToInt32(item["width"])
+                });
+            }
+
+
+            Console.WriteLine(titles.ToString());
+            Console.ReadKey();
+        }
+    }
+}
