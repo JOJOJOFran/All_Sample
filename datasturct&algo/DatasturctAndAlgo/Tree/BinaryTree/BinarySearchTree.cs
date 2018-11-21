e6f66499fe9ec22778aa74e2ccf292c4d0195d72
fran <zhanfan930818@hotmail.com>
2018-11-21 22:38:48 +0800

    添加二叉查找树

---
+++ datasturct&algo/DatasturctAndAlgo/Tree/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatasturctAndAlgo.Tree.BinaryTree
+{
+    /// <summary>
+    /// 二叉查找树
+    /// </summary>
+    public abstract class BinarySearchTree 
+    {
+        public SearchTreeNode Root { get; private set; }
+
+        public BinarySearchTree(int value)
+        {
+            Root = new SearchTreeNode(value);
+        }
+
+        public void Insert(int data)
+        {
+            if (Root == null)
+            {
+                Root = new SearchTreeNode(data);
+            }
+
+            var node = Root;
+            while (node != null)
+            {
+                //这里暂且把等于该节点的值也插入左节点
+                if (data < node.Value)
+                {
+                    if (node.LeftNode == null)
+                    {
+                        node.LeftNode = new SearchTreeNode(data);
+                        return;
+                    }
+                    node = node.LeftNode;
+
+                }
+                else if (data == node.Value && node.IsDeleted)
+                {
+                    node.IsDeleted = false;
+                }
+                else if (data > node.Value)
+                {
+                    if (node.RightNode == null)
+                    {
+                        node.RightNode = new SearchTreeNode(data);
+                        return;
+                    }
+                    node = node.RightNode;
+                }
+               
+            }             
+        }
+
+        public SearchTreeNode Find(int data)
+        {
+            if (Root == null)
+                return null;
+
+            var node = Root;
+            while (node != null)
+            {
+                if (data < node.Value)
+                {
+                    node = node.LeftNode;
+                }
+                else if (data > node.Value)
+                {
+                    node = node.RightNode;
+                }
+                else if(data==node.Value&&!node.IsDeleted)
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 标记删除
+        /// </summary>
+        /// <param name="data"></param>
+        public void MarkDelete(int data)
+        {
+            if (Root == null)
+                return;
+
+            var node = Root;
+            while (node != null)
+            {
+                if (data < node.Value)
+                {
+                    node = node.LeftNode;
+                }
+                else if (data == node.Value && !node.IsDeleted)
+                {
+                    node.IsDeleted = true;
+                }
+                else if(data >node.Value)
+                {
+                    node = node.RightNode;
+                }
+            }
+        }
+
+        public void Delete(int data)
+        {
+            if (Root == null)
+                return;
+
+            var node = Root;
+            var nextNode = node;
+            while (node != null)
+            {
+                if (data < node.Value)
+                {
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ datasturct&algo/DatasturctAndAlgo/Tree/BinaryTree/BranchNode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatasturctAndAlgo.Tree.BinaryTree
+{
+    public interface ITreeNodeOperation
+    {
+        void SetLeftNode(int value);
+        void SetRightNode(int value);
+
+    }
+
+    public abstract class BaseTreeNode : ITreeNodeOperation
+    {
+        public abstract void SetLeftNode(int value);
+
+
+        public abstract void SetRightNode(int value);
+
+    }
+
+    public class BranchNode : BaseTreeNode
+    {
+        public BranchNode LeftNode { get; private set; }
+        public BranchNode RightNode { get; private set; }
+        public int Value { get; set; }
+
+        public BranchNode(int value)
+        {
+            Value = value;
+        }
+
+        public sealed override void SetLeftNode(int value)
+        {
+            LeftNode = new BranchNode(value);
+        }
+
+
+        public sealed override void SetRightNode(int value)
+        {
+            LeftNode = new BranchNode(value);
+        }
+    }
+
+    public class SearchTreeNode
+    {
+        private SearchTreeNode _leftNode;
+        private SearchTreeNode _rightNode;
+        public SearchTreeNode LeftNode
+        {
+            get { return _leftNode; }
+            set
+            {
+                if (value.Value <=this.Value)
+                    _leftNode = value;
+                else
+                    Console.WriteLine("插入值:{0}大于当前节点值，不允许插入左子节点", value);
+            }
+        }
+
+        public SearchTreeNode RightNode
+        {
+            get { return _rightNode; }
+            set
+            {
+                if (value.Value > this.Value)
+                    _rightNode = value;
+                else
+                    Console.WriteLine("插入值:{0}小于当前节点值，不允许插入右子节点", value);
+            }
+        }
+        public int Value { get; set; }
+
+        public SearchTreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
