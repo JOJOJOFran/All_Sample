7d73c26c29790dad0827bdbcfcf3aeb9d132970c
fran <zhanfan930818@hotmail.com>
2018-12-03 22:01:28 +0800

    前台后台线程测试

--- clr sample/async threading/Interlocked/Program.cs
+++ clr sample/async threading/Interlocked/Program.cs
@@ -7,17 +7,55 @@ namespace Interlocked_sample
     {
         static void Main(string[] args)
         {
-            int test = 4;
-            while (test >= 0)
-            {
-                test--;
-                Console.WriteLine(test);
-            }
-                
-            
+
+            Console.WriteLine("请输入对应数字执行对应的方法：");
+            Console.WriteLine("1.执行InterLockedTest ");
+            Console.WriteLine("2.执行EasyThreadAboutBackOrFontTest ");
+            FuncProvider(Console.ReadLine());
+
+        }
+
+        static void InterLockedTest()
+        {
             MultiWebRequests a = new MultiWebRequests();
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        }
+
+        static void EasyThreadAboutBackOrFontTest()
+        {
+            Thread t = new Thread(Worker);
+            // t.IsBackground = true;
+            //如果是前台线程，会执行Worker里面的方法
+            //如果是后台线程，会直接结束
+            t.Start();
+
+
+            Console.WriteLine("Return from the Main ");
+        }
+
+        static void Worker()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Return from Worker");
+        }
+
+
+        static void FuncProvider(string type)
+        {
+            switch (type)
+            {
+                case "1":
+                    InterLockedTest();
+                    FuncProvider(Console.ReadLine());
+                    break;
+                case "2":
+                    EasyThreadAboutBackOrFontTest();
+                    FuncProvider(Console.ReadLine());
+                    break;
+                case "exit":
+                    Console.WriteLine("bye");
+                    Thread.Sleep(1000);
+                    break;
+            }
         }
     }
 }
