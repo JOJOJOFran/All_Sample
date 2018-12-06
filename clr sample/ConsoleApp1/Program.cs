f5e91d15ed2e0f3bb79da153364c62757ab7a84e
fran <zhanfan930818@hotmail.com>
2018-12-06 23:13:09 +0800

    添加Task

--- clr sample/ConsoleApp1/Program.cs
+++ clr sample/ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MultiThreadDemo;
+using System;
 using System.Collections.Concurrent;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -18,6 +20,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("1.执行上下文测试代码");
             Console.WriteLine("2.执行取消异步操作代码");
+            Console.WriteLine("3.执行Task示例");
         }
 
         static void FunctionProvider(string type)
@@ -25,39 +28,18 @@ namespace ConsoleApp1
             switch (type)
             {
                 case "1":
-                    ExecutionContextTest();
+                    ThreadContextDemo.ExecutionContextTest();
                     break;
                 case "2":
                     CancellationDemo.Go();
                     break;
+                case "3":
+                    TaskDemo.Go();
+                    break;
             }
         }
 
-        /// <summary>
-        /// 执行上下文测试代码
-        /// </summary>
-        static void ExecutionContextTest()
-        {
-            //将一些数据放到主线程的逻辑调用上下文中
-            CallContext.SetData("Name", "Fran");
-
-            //初始化要由一个线程池线程做的一些工作
-            //线程池线程能访问逻辑调用上下文数据
-            ThreadPool.QueueUserWorkItem(state => Console.WriteLine("Name={0}", CallContext.GetData("Name")));
-            Thread.Sleep(1000);
-            //阻止线程上下文流动
-            ExecutionContext.SuppressFlow();
-            Console.WriteLine("现在我们阻止线程上下文流动，ExecutionContext的IsFlowSupper值为:{0}", ExecutionContext.IsFlowSuppressed());
-            CallContext.SetData("Age", "25");
-            ThreadPool.QueueUserWorkItem(ApartmentState => Console.WriteLine("Age={0}", CallContext.GetData("Age") ?? "?"));
-            Thread.Sleep(1000);
-            //恢复线程上下文流动
-            ExecutionContext.RestoreFlow();
-            Console.WriteLine("现在我们恢复线程上下文流动，ExecutionContext的IsFlowSupper值为:{0}", ExecutionContext.IsFlowSuppressed());
-            ThreadPool.QueueUserWorkItem(ApartmentState => Console.WriteLine("Age={0}", CallContext.GetData("Age") ?? "?"));
-
-
-        }
+      
 
         
 
---
+++ clr sample/ConsoleApp1/ThreadContextDemo.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadDemo
+{
+    public static class ThreadContextDemo
+    {
+        /// <summary>
+        /// 执行上下文测试代码
+        /// </summary>
+        public static void ExecutionContextTest()
+        {
+            //将一些数据放到主线程的逻辑调用上下文中
+            CallContext.SetData("Name", "Fran");
+
+            //初始化要由一个线程池线程做的一些工作
+            //线程池线程能访问逻辑调用上下文数据
+            ThreadPool.QueueUserWorkItem(state => Console.WriteLine("Name={0}", CallContext.GetData("Name")));
+            Thread.Sleep(1000);
+            //阻止线程上下文流动
+            ExecutionContext.SuppressFlow();
+            Console.WriteLine("现在我们阻止线程上下文流动，ExecutionContext的IsFlowSupper值为:{0}", ExecutionContext.IsFlowSuppressed());
+            CallContext.SetData("Age", "25");
+            ThreadPool.QueueUserWorkItem(ApartmentState => Console.WriteLine("Age={0}", CallContext.GetData("Age") ?? "?"));
+            Thread.Sleep(1000);
+            //恢复线程上下文流动
+            ExecutionContext.RestoreFlow();
+            Console.WriteLine("现在我们恢复线程上下文流动，ExecutionContext的IsFlowSupper值为:{0}", ExecutionContext.IsFlowSuppressed());
+            ThreadPool.QueueUserWorkItem(ApartmentState => Console.WriteLine("Age={0}", CallContext.GetData("Age") ?? "?"));
+
+
+        }
+    }
+}
